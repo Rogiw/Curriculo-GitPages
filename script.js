3f923c4f7b33473d65225f208afeb5b6edfa2d11
White <111619213+Rogiw@users.noreply.github.com>
2025-11-22 20:08:58 -0300

    fixing medias

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const profileBtn = document.querySelector('.profile-btn');
 const sidebar = document.querySelector('.sidebar');
 const profileImg = document.querySelector('.profile-img');
+const sideimg = document.querySelector('.side-img')
 
-profileBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('show');      // abre/fecha sidebar
-    profileBtn.classList.toggle('hide-img'); // esconde/mostra imagem
-});
+
+function toggleSidebar(event) {
+    event.preventDefault(); // Prevenir o comportamento padrão do link
+    sidebar.classList.toggle('active');
+    profileBtn.classList.toggle('hide-img');
+
+     if (window.matchMedia("(max-width: 600px)").matches) {
+        // Assim nao fica cagado quando você esta em um pc, porque essa bosta puxa tudo, nao só o sidebar
+        sidebar.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+}
+
+profileBtn.addEventListener('click', toggleSidebar)
+sideimg.addEventListener('click', toggleSidebar)
+
